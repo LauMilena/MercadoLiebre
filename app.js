1bc8c0aee94d4258454c2b990e5f8844b0bb7dc5
LauMilena <lauramile05@gmail.com>
2024-02-21 00:41:48 -0500

    avances mercadoliebre

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 })
 
+app.get("/register", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
 
+app.post("/register", function(req, res){
+    res.send("Se registró con exito")
+})
+
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.post("/login", function(req, res){
+    res.send("Bienvenido")
+})
